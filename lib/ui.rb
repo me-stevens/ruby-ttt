19d8b434606f02cc768caf1ee81a1a7bbf9447fe
private <user@example.com>
2016-02-25 14:25:57 +0000

    Add method to read a cell index from the user input.

--- lib/ui.rb
+++ lib/ui.rb
@@ -1,7 +1,10 @@
 class UI
 
-  def initialize(console)
-    @console = console
+  ERROR_WRONG_INPUT = "ERROR: Wrong input. Please try again: "
+
+  def initialize(console, validator)
+    @console   = console
+    @validator = validator
   end
 
   def print_board(board)
@@ -9,8 +12,27 @@ class UI
     (0...board.size).each { |i| println(board.row(i).join("\t")) }
   end
 
+  def read_cell(board)
+    cell = ""
+    loop do
+      cell = read
+      break if valid_cell?(board.total_cells, cell)
+      error_wrong_input
+    end
+    Integer(cell) - 1
+  end
+
   private
 
+  attr_reader :console, :validator
+
+  def read
+    console.read.chomp
+  end
+
+  def print(message)
+    console.write(message)
+  end
 
   def println(message)
     console.writeln(message)
@@ -19,6 +41,14 @@ class UI
   def format_cells(board)
     i = 0
     Board.new(board.all.collect { |cell| i += 1; cell == :E ? i : cell }.to_a)
+  end
+
+  def valid_cell?(total_cells, cell)
+    validator.valid_cell?(total_cells, cell)
+  end
+
+  def error_wrong_input
+    print(ERROR_WRONG_INPUT)
   end
 
 end
--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -1,4 +1,5 @@
 require 'console'
+require 'validator'
 require 'ui'
 require 'board'
 require 'marks'
@@ -9,16 +10,16 @@ describe UI do
   let(:o) {Marks::O}
   let(:e) {Marks::E}
 
+  let(:board)  {Board.new([
+    [x, o, x],
+    [o, x, o],
+    [o, o, x]
+  ])}
   let(:input)  {StringIO.new}
   let(:output) {StringIO.new}
-  let(:ui)     {described_class.new(Console.new(input, output))}
+  let(:ui)     {described_class.new(Console.new(input, output), Validator.new)}
 
   it "prints a board with format" do
-    board = Board.new([
-      [x, o, x],
-      [o, x, o],
-      [o, o, x]
-    ])
     ui.print_board(board)
     expect(output.string).to eq("X\tO\tX\nO\tX\tO\nO\tO\tX\n")
   end
@@ -27,6 +28,23 @@ describe UI do
     board = Board.new([[e, e, e], [e, e, e], [e, e, e]])
     ui.print_board(board)
     expect(output.string).to eq("1\t2\t3\n4\t5\t6\n7\t8\t9\n")
+  end
+
+  it "reads a cell index from prompt" do
+    input.string = "1\n"
+    expect(ui.read_cell(board)).to eq(0)
+  end
+
+  it "prints an error message if wrong cell index" do
+    input.string = "asdf\n1\n"
+    ui.read_cell(board)
+    expect(output.string).to include(UI::ERROR_WRONG_INPUT)
+  end
+
+  it "keeps asking until valid cell index" do
+    input.string = "asdf\n-1\n0\n1000\n1\n"
+    expect(ui.read_cell(board)).to eq(0)
+    expect(output.string.scan(UI::ERROR_WRONG_INPUT).size).to eq(4)
   end
 
 end
