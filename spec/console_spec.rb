486839b17a7ed426e35808a2eccf568cc165c2f8
private <user@example.com>
2016-02-25 09:31:32 +0000

    Add the console class.

---
+++ spec/console_spec.rb
@@ -0,0 +1,28 @@
+require 'console'
+
+describe Console  do
+
+  it "prints to the console" do
+    output  = StringIO.new
+    console = described_class.new(nil, output)
+    console.write("boo")
+		
+    expect(output.string).to eq("boo")
+  end
+
+  it "prints a line to the console" do
+    output  = StringIO.new
+    console = described_class.new(nil, output)
+    console.writeln("boo")
+
+    expect(output.string).to eq("boo\n")
+  end
+
+  it "reads from console" do
+    input   = StringIO.new("hello\n")
+    console = described_class.new(input, nil)
+		
+    expect(console.read()).to eq("hello\n")
+  end
+
+end
