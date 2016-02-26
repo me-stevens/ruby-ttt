68fbd7e4544cd3f23f1f6bcd6de704e29afa2d3e
private <user@example.com>
2016-02-26 10:55:45 +0000

    Add a class to define what happens in a turn.

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+class Turn
+
+  def initialize(ui)
+    @ui = ui
+  end
+
+  def play(board, mark)
+    @board = board
+    print_board
+    place_mark(read_cell, mark)
+  end
+
+  private
+
+  attr_reader :ui, :board
+
+  def print_board
+    ui.print_board(board)
+  end
+
+  def read_cell
+    ui.read_cell(board)
+  end
+
+  def place_mark(cell, mark)
+    board.place_mark(cell, mark)
+  end
+
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,38 @@
+require 'ui'
+require 'board'
+require 'turn'
+
+describe Turn do
+
+  let(:ui)    {instance_double(UI).as_null_object}
+  let(:board) {instance_double(Board).as_null_object}
+  let(:turn)  {described_class.new(ui)}
+  let(:mark)  {:X}
+
+  before :each do
+    allow(ui).to receive(:read_cell).and_return(0)
+  end
+
+  it "prints the board" do
+    turn.play(board, mark)
+    expect(ui).to have_received(:print_board).with(board)
+  end
+
+  it "asks the user for a cell to place the mark" do
+    turn.play(board, mark)
+    expect(ui).to have_received(:read_cell).with(board)
+  end
+
+  it "places the mark in the board" do
+    turn.play(board, mark)
+    expect(board).to have_received(:place_mark).with(0, mark)
+  end
+
+  it "returns the updated board" do
+    board = Board.new([[:E, :E, :E], [:E, :E, :E], [:E, :E, :E]])
+    rows  = [[:X, :E, :E], [:E, :E, :E], [:E, :E, :E]]
+    turn  = described_class.new(ui)
+    expect(turn.play(board, mark).all).to eq(Matrix.rows(rows))
+  end
+
+end
