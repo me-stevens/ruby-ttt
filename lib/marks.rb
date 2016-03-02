938cbf11a66d3d809c7219b253f30469e5fbd5fb
private <user@example.com>
2016-03-02 20:23:20 +0000

    Put back the opponent method in the marks.

--- lib/marks.rb
+++ lib/marks.rb
@@ -2,4 +2,9 @@ module Marks
   X = :X
   O = :O
   E = :E
+
+  def self.opponent(mark)
+    mark == X ? O : X
+  end
+
 end
---
+++ spec/marks_spec.rb
@@ -0,0 +1,15 @@
+require 'marks'
+
+describe Marks do
+
+  it "swaps X to O" do
+    mark = Marks::X
+    expect(Marks.opponent(mark)).to eq(Marks::O)
+  end
+
+  it "swaps O to X" do
+    mark = Marks::O
+    expect(Marks.opponent(mark)).to eq(Marks::X)
+  end
+
+end
