ee0b3f103c339166fd1e053ad68ebb4f276805d8
private <user@example.com>
2016-03-04 01:14:37 +0000

    Split the minimax algorithm and test the tiny parts.

--- lib/robot_player.rb
+++ lib/robot_player.rb
@@ -1,18 +1,54 @@
+require 'marks'
 require 'player'
 
 class RobotPlayer < Player
 
   def make_move(board)
     super
-    read_cell(board)
+    minimax(board, mark).first
+  end
+
+  def minimax(current_board, current_player)
+    index = initialize_index(current_board)
+    score = initialize_score(current_player)
+
+
+    [index, score]
+  end
+
+  def initialize_index(current_board) # not sure about this
+    current_board.cell_count
+  end
+
+  def initialize_score(current_player)
+    current_player == mark ? -100 : 100
+  end
+
+  def empty_cells(current_board)
+    current_board.indexes_of(Marks::E)
+  end
+
+  def game_is_not_over?(temp_board, current_player)
+    !game_over?(temp_board, current_player)
+  end
+
+  def heuristics(temp_board, current_player)
+    if temp_board.win?(mark)
+      current_player == mark ? 10 : -10
+    else
+      0
+    end
+  end
+
+  def better_score?(current_player, temp_score, score)
+    (current_player == mark && temp_score > score) ||
+    (current_player != mark && temp_score < score)
   end
 
   private
 
-  def read_cell(board)
-    minimax(board, mark)
+  def game_over?(temp_board, current_player)
+    temp_board.win?(current_player) || temp_board.full?(Marks::E)
   end
 
-  def minimax(current_board, current_player)
-  end
 end
--- spec/robot_player_spec.rb
+++ spec/robot_player_spec.rb
@@ -5,6 +5,11 @@ require 'ui'
 describe RobotPlayer do
 
   let(:ui)    {instance_double(UI).as_null_object}
+  let(:board) {Board.new([
+    [:X, :O, :X],
+    [:O, :X, :O],
+    [:O, :E, :X]
+  ])}
   let(:robot) {described_class.new(ui, :O)}
 
   it "subclasses the player class so gets the mark" do
@@ -12,9 +17,74 @@ describe RobotPlayer do
   end
 
   it "prints the turn" do
-    board = Board.new([[:E, :E, :E], [:E, :E, :E], [:E, :E, :E]])
     robot.make_move(board)
     expect(ui).to have_received(:print_turn).with(:O)
+  end
+
+  it "initializes the index to the board cell count in every change of player" do
+    expect(robot.initialize_index(board)).to eq(9)
+  end
+
+  it "initializes the score to -100 if current player is robot" do
+    expect(robot.initialize_score(:O)).to eq(-100)
+  end
+
+  it "initializes the score to 100 if current player is robot's opponent" do
+    expect(robot.initialize_score(:X)).to eq(100)
+  end
+
+  it "returns the empty cell indexes of the board" do
+    expect(robot.empty_cells(board)).to eq([7])
+  end
+
+  it "game is not over if board is not full or has no winner" do
+    expect(robot.game_is_not_over?(board, :O)).to eq(true)
+  end
+
+  it "game is over if board has winner" do
+    expect(robot.game_is_not_over?(board, :X)).to eq(false)
+  end
+
+  it "heuristics are zero if there is no robot win" do
+    expect(robot.heuristics(board, :O)).to eq(0)
+  end
+
+  it "heuristics are 10 for the robot if there is a robot win" do
+    board = Board.new([[:O, :O, :O], [:E, :E, :E], [:E, :E, :E]])
+    expect(robot.heuristics(board, :O)).to eq(10)
+  end
+
+  it "heuristics are -10 for the opponent if there is a robot win" do
+    board = Board.new([[:O, :O, :O], [:E, :E, :E], [:E, :E, :E]])
+    expect(robot.heuristics(board, :X)).to eq(-10)
+  end
+
+  it "score is better if its a robot and temp score is bigger than current score" do
+    temp_score = 10
+    score      = 0
+    expect(robot.better_score?(:O, temp_score, score)).to eq(true)
+  end
+
+  it "score is not better if its a robot and temp score is lower than current score" do
+    temp_score = 0
+    score      = 10
+    expect(robot.better_score?(:O, temp_score, score)).to eq(false)
+  end
+
+  it "score is better if its not a robot and temp score is lower than current score" do
+    temp_score = 0
+    score      = 10
+    expect(robot.better_score?(:X, temp_score, score)).to eq(true)
+  end
+
+  it "score is not better if its not a robot and temp score is higher than current score" do
+    temp_score = 10
+    score      = 0
+    expect(robot.better_score?(:X, temp_score, score)).to eq(false)
+  end
+
+  it "should place the mark in the only available spot" do
+    expect(robot.make_move(board)).to eq(7)
   end
 
 end
